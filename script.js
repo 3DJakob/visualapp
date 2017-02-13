182f4bc98bbb76a0194c5c02709a282c9158fdb9
Jakob Unnebäck <jakob@folkdatorn.se>
2017-02-13 22:35:59 +0100

    Error feedback + multiple answers

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 var mass = ['kg', 'g']
-
 var massExamples = [{
   'title': 'Golfballs',
   'measurement': '0.04593'
-// }, {
-//   'title': 'Elephants',
-//   'measurement': '5500'
+}, {
+  'title': 'Elephants',
+  'measurement': '5500'
 }]
 
 function submit () {
+  document.getElementById('result').innerHTML = ''
+  document.getElementById('unit').style.backgroundColor = 'rgba(255, 0, 0, 0.4)'
+  document.getElementById('number').style.backgroundColor = 'rgba(255, 255, 255, 1)'
   var inputUnit = document.getElementById('unit').value
   var inputNumber = document.getElementById('number').value
 
+  if (isNaN(inputNumber)) {
+    document.getElementById('number').style.backgroundColor = 'rgba(255, 0, 0, 0.3)'
+  }
 
   mass.forEach(function (item) {
-    if (item == inputUnit) {
+    if (item === inputUnit) {
       for (var i = 0; i < massExamples.length; i++) {
-        document.getElementById('result').innerHTML = inputNumber / massExamples[i].measurement + ' ' + massExamples[i].title
+        if (isNaN(inputNumber) === false) {
+          document.getElementById('result').innerHTML += '<h3>' + inputNumber / massExamples[i].measurement + ' ' + massExamples[i].title + '</h3>'
+        }
+        document.getElementById('unit').style.backgroundColor = 'rgba(255, 255, 255, 1)'
       }
-    } else {
-      // document.getElementById('result').innerHTML = 'Unsupported unit!'
     }
-
   })
 }
