4d9cfead74c69ddf64ff1595896baf32fb489165
Jakob Unnebäck <jakob@folkdatorn.se>
2017-02-14 16:09:39 +0100

    time + length quantities

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 const units = new Map([
+  ['pc', { quantityName: 'length', multiplier: 30856775800000000 }],
+  ['ly', { quantityName: 'length', multiplier: 9460528400000000 }],
+  ['mil', { quantityName: 'length', multiplier: 10000 }],
+  ['mile', { quantityName: 'length', multiplier: 1609.344 }],
+  ['km', { quantityName: 'length', multiplier: 1000 }],
+  ['m', { quantityName: 'length', multiplier: 1 }],
+  ['yard', { quantityName: 'length', multiplier: 0.9144 }],
+  ['yards', { quantityName: 'length', multiplier: 0.9144 }],
+  ['foot', { quantityName: 'length', multiplier: 0.3048 }],
+  ['feet', { quantityName: 'length', multiplier: 0.3048 }],
+  ['dm', { quantityName: 'length', multiplier: 0.1 }],
+  ['inch', { quantityName: 'length', multiplier: 0.0254 }],
+  ['cm', { quantityName: 'length', multiplier: 0.01 }],
+  ['mm', { quantityName: 'length', multiplier: 0.001 }],
+  ['Å', { quantityName: 'length', multiplier: 0.0000000001 }],
+
   ['t', { quantityName: 'mass', multiplier: 1000 }],
+  ['st', { quantityName: 'mass', multiplier: 6.35029318 }],
   ['kg', { quantityName: 'mass', multiplier: 1 }],
+  ['lbs', { quantityName: 'mass', multiplier: 0.45359237 }],
   ['g', { quantityName: 'mass', multiplier: 0.001 }],
   ['u', { quantityName: 'mass', multiplier: 0.00000000000000000000000000166 }],
+
+  ['year', { quantityName: 'time', multiplier: 31556926 }],
+  ['years', { quantityName: 'time', multiplier: 31556926 }],
+  ['week', { quantityName: 'time', multiplier: 604800 }],
+  ['weeks', { quantityName: 'time', multiplier: 604800 }],
+  ['day', { quantityName: 'time', multiplier: 86400 }],
+  ['days', { quantityName: 'time', multiplier: 86400 }],
+  ['h', { quantityName: 'time', multiplier: 3600 }],
+  ['min', { quantityName: 'time', multiplier: 60 }],
   ['s', { quantityName: 'time', multiplier: 1 }]
 ])
 
